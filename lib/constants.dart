fb0c59245d671887b56f6a6fb501c840c19a3bde
Marco <marcopg4@gmail.com>
2022-12-22 13:09:22 +0100

    fixed typo

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
 const MaterialColor MAIN_COLOR = Colors.green;
-const String FCM_TOPIC = 'smart_notificaitons';
+const String FCM_TOPIC = 'smart_notifications';
